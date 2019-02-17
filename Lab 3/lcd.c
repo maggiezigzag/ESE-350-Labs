60fdef7f90a1bd17454478437d6f3b55bf4b00fb
Maggie Zhang <maggiezigzag@gmail.com>
2019-02-17 14:37:36 -0500

    Update lcd.c

--- Lab 3/lcd.c
+++ Lab 3/lcd.c
@@ -4,11 +4,14 @@
 #include <util/delay.h>
 #include <avr/pgmspace.h>
 #include "lcd.h"
+//#include "uart.h"
 
 #define SID_DDR DDRD
 #define SID_PIN PIND
 #define SID_PORT PORTD
 #define SID 6
+
+
 
 #define SCLK_DDR DDRD
 #define SCLK_PIN PIND
@@ -29,6 +32,7 @@
 #define CS_PIN PIND
 #define CS_PORT PORTD
 #define CS 2
+
 
 uint8_t is_reversed = 0;
 
@@ -496,44 +500,176 @@ void drawchar(uint8_t *buff, uint8_t x, uint8_t line, uint8_t c) {
 
 // the most basic function, set a single pixel
 void setpixel(uint8_t *buff, uint8_t x, uint8_t y, uint8_t color) {
+	if((x >= LCDWIDTH) || (y >= LCDHEIGHT)){
+		return;
+	}
 	
+	if(color)
+		buff[x+(y/8)*128] |= (1<<(7-y%8)); 
+	else
+		buff[x+(y/8)*128] &= ~(1<<(7-y%8)); 	
 }
 
 // function to clear a single pixel
 void clearpixel(uint8_t *buff, uint8_t x, uint8_t y) {
-	
+
+		buff[x+(y/8)*128] &= ~(1<<(7-(y%8)));
 }
 
 // function to write a string on the lcd
 void drawstring(uint8_t *buff, uint8_t x, uint8_t line, uint8_t *c) {
-	
+	while(c[0] != 0){
+		//uart_putchar(c[0]);
+		drawchar(buff, x, line, c[0]);
+		c++;
+		
+		//Safeguard against running out of x space and y space.
+		x+=6;
+		
+		if(x+6 >= LCDWIDTH){
+			x=0; ///ran out of space on the line
+			line++;
+		}
+		
+		if(line>= (LCDHEIGHT/8)){
+			return;
+		}
+	}
 }
 
 // use bresenham's algorithm to write this function to draw a line
 void drawline(uint8_t *buff,uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1,uint8_t color) {
+	
+	uint8_t steep = abs(y1 - y0) > abs(x1 - x0);
+	if (steep) {
+		swap(x0, y0);
+		swap(x1, y1);
+	}
 
+	if (x0 > x1) {
+		swap(x0, x1);
+		swap(y0, y1);
+	}
+
+	uint8_t dx, dy;
+	dx = x1 - x0;
+	dy = abs(y1 - y0);
+
+	int8_t err = dx / 2;
+	int8_t ystep;
+
+	if (y0 < y1) {
+		ystep = 1;
+		} else {
+	ystep = -1;}
+
+	for (; x0<x1; x0++) {
+		if (steep) {
+			setpixel(buff, y0, x0, color);
+			} else {
+			setpixel(buff, x0, y0, color);
+		}
+		err -= dy;
+		if (err < 0) {
+			y0 += ystep;
+			err += dx;
+		}
+	}
 }
 
 // function to draw a filled rectangle
 void fillrect(uint8_t *buff,uint8_t x, uint8_t y, uint8_t w, uint8_t h,uint8_t color) {
+		for (uint8_t i=x; i<x+w; i++) {
+			for (uint8_t j=y; j<y+h; j++) {
+				setpixel(buff, i, j, color);
+			}
+		}
 	
 }
 
 
 // function to draw a rectangle
 void drawrect(uint8_t *buff,uint8_t x, uint8_t y, uint8_t w, uint8_t h,uint8_t color) {
-	
+	  for (uint8_t i=x; i<x+w; i++) {
+		  setpixel(buff, i, y, color);
+		  setpixel(buff, i, y+h-1, color);
+	  }
+	  for (uint8_t i=y; i<y+h; i++) {
+		  setpixel(buff, x, i, color);
+		  setpixel(buff, x+w-1, i, color);
+	  }
 }
 
 
 // function to draw a circle
 void drawcircle(uint8_t *buff,uint8_t x0, uint8_t y0, uint8_t r,uint8_t color) {
+	  int8_t f = 1 - r;
+	  int8_t ddF_x = 1;
+	  int8_t ddF_y = -2 * r;
+	  int8_t x = 0;
+	  int8_t y = r;
+
+	  setpixel(buff, x0, y0+r, color);
+	  setpixel(buff, x0, y0-r, color);
+	  setpixel(buff, x0+r, y0, color);
+	  setpixel(buff, x0-r, y0, color);
+
+	  while (x<y) {
+		  if (f >= 0) {
+			  y--;
+			  ddF_y += 2;
+			  f += ddF_y;
+		  }
+		  x++;
+		  ddF_x += 2;
+		  f += ddF_x;
+		  
+		  setpixel(buff, x0 + x, y0 + y, color);
+		  setpixel(buff, x0 - x, y0 + y, color);
+		  setpixel(buff, x0 + x, y0 - y, color);
+		  setpixel(buff, x0 - x, y0 - y, color);
+		  
+		  setpixel(buff, x0 + y, y0 + x, color);
+		  setpixel(buff, x0 - y, y0 + x, color);
+		  setpixel(buff, x0 + y, y0 - x, color);
+		  setpixel(buff, x0 - y, y0 - x, color);
+		  
+	  }
 	
+		
+	 
 }
 
 
 // function to draw a filled circle
 void fillcircle(uint8_t *buff,uint8_t x0, uint8_t y0, uint8_t r,uint8_t color) {
-	
-}
+	int8_t f = 1 - r;
+	int8_t ddF_x = 1;
+	int8_t ddF_y = -2 * r;
+	int8_t x = 0;
+	int8_t y = r;
 
+	for (uint8_t i=y0-r; i<=y0+r; i++) {
+		setpixel(buff, x0, i, color);
+	}
+
+	while (x<y) {
+		if (f >= 0) {
+			y--;
+			ddF_y += 2;
+			f += ddF_y;
+		}
+		x++;
+		ddF_x += 2;
+		f += ddF_x;
+		
+		for (uint8_t i=y0-y; i<=y0+y; i++) {
+			setpixel(buff, x0+x, i, color);
+			setpixel(buff, x0-x, i, color);
+		}
+		for (uint8_t i=y0-x; i<=y0+x; i++) {
+			setpixel(buff, x0+y, i, color);
+			setpixel(buff, x0-y, i, color);
+		}
+	}
+}
